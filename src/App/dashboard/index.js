79d351a4b1bcf26150123ae8edb13cdd0395745a
admin <sivakumar.murugesan@prabalinfo.co.in>
2020-06-15 14:57:01 +0530

    view profile

--- src/App/dashboard/index.js
+++ src/App/dashboard/index.js
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 import { GrHelp } from 'react-icons/gr';
 import logo from '../../assets/logo.png';
 
+const private_img = require('../../assets/user-profile.jpg');
+const workgroup_profile =
+	'https://images.unsplash.com/photo-1581360742512-021d5b2157d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=50&q=80';
 export default function Dashboard({ history, authObj }) {
 	const [brandLinkWidth, setBrandLinkWidth] = React.useState(0);
+	const [isDropdownVisible, setIsDropdownVisible] = React.useState(false);
 
 	const menu_bg_color = '#0F1662';
 	const menu_font_color = 'white';
@@ -98,20 +102,97 @@ export default function Dashboard({ history, authObj }) {
 			padding: 0 40px;
 			justify-content: space-between;
 			position: relative;
+			
 			.profile-info {
+				cursor: pointer;
 				p {
 					margin: 0;
+					font-size: 90%;
 				}
 			}
 			.profile-picture {
 				position: absolute;
 				right: 40px;
 				bottom: -15px;
+				cursor: pointer;
 				img {
 					width: 55px;
 					height: 55px;
 					border-radius: 50%;
 					border: 4px solid ${top_menu_dropdown_bg_color};
+				}
+			}
+			.show-dropdown {
+				box-sizing: border-box;
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 99%;
+				min-height: 350px;
+				background: white;
+				border: 2px solid #999;
+				border-radius: 10px;
+				padding: 10px;
+				margin: auto;
+				color: #666;
+				font-size: 90%;
+				.profile-picture-block {				
+					display: flex;
+					justify-content: flex-end;
+					width: 100%;			
+					span {
+						border-radius: 50%;
+						border: 3px solid #999;
+						width: 40px;
+						height: 40px;
+						overflow: hidden;
+						img {
+							width: 100%;
+						}
+					}
+				}
+				.persona-name {
+					border-bottom: 2px solid #999;
+					p {
+						margin: 5px 0;
+					}
+				}
+				.current-persona-info {					
+					border-bottom: 2px solid #999;		
+					p {
+						margin: 5px 0;
+					}
+				}
+				.personas-container {
+					padding: 0;
+					.persona-item {
+						display: flex;
+						padding: 5px 0;
+						cursor: pointer;
+						:hover {
+							background: #eee;
+						}
+						.persona-item-image {
+						border-radius: 50%;
+						border: 3px solid #999;
+						width: 40px;
+						height: 40px;
+						overflow: hidden;
+						img {
+							width: 100%;
+						}
+					}
+					.persona-item-text {
+						padding-left: 10px;
+					}
+					}
+				}
+				button {
+					border: 0;
+					background: 0;
+					color: #666;
+					padding: 5px;
+					cursor: pointer;					
 				}
 			}
 		}
@@ -160,27 +241,65 @@ export default function Dashboard({ history, authObj }) {
 					</ul>
 				</nav>
 				<div className="navbar-dropdown">
-					<div className="profile-info">
+					<div
+						className="profile-info"
+						onClick={() => setIsDropdownVisible(true)}
+					>
 						<p>Welcome !</p>
-						<p>welcome@gmail.com</p>
+						<p>username@mail.com</p>
 					</div>
-					<div className="profile-picture">
+					<div
+						className="profile-picture"
+						onClick={() => setIsDropdownVisible(true)}
+					>
 						<span>
-							<img src={require('../../assets/user-profile.jpg')} />
+							<img src={private_img} alt="..." />
 						</span>
 					</div>
+					{isDropdownVisible && (
+						<div
+							className="show-dropdown"
+							onMouseLeave={() => setIsDropdownVisible(false)}
+						>
+							<div className="profile-picture-block">
+								<span>
+									<img src={private_img} alt="..." />
+								</span>
+							</div>
+							<div className="persona-name">
+								<p>Private</p>
+							</div>
+							<div className="current-persona-info">
+								<p>username</p>
+								<p>USER_ID 481490</p>
+								<p>Last logged in @ 12/07/2019 - 12:05</p>
+							</div>
+							<ul className="personas-container">
+								<li className="persona-item">
+									<span className="persona-item-image">
+										<img src={private_img} alt="..." />
+									</span>
+									<p className="persona-item-text">Private</p>
+								</li>
+								<li className="persona-item">
+									<span className="persona-item-image">
+										<img src={workgroup_profile} alt="..." />
+									</span>
+									<p className="persona-item-text">Workgroup Name</p>
+								</li>
+							</ul>
+							<button
+								onClick={() => {
+									authObj.authenticate(false);
+									history.push('/');
+								}}
+							>
+								Log Out
+							</button>
+						</div>
+					)}
 				</div>
 			</TopMenu>
-			<br />
-
-			<button
-				onClick={() => {
-					authObj.authenticate(false);
-					history.push('/');
-				}}
-			>
-				Logout
-			</button>
 		</>
 	);
 }
