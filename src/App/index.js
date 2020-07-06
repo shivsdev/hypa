933cabf4048ce630a702088259f554a73b4491d9
shivsdev <sivakumar.murugesan@prabalinfo.co.in>
2020-07-06 11:38:43 +0530

    styles api issue

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,24 @@
-import React, { useEffect } from "react";
-import { Route, Redirect } from "react-router-dom";
-import axios from "axios";
-import Signin from "./signin";
-import Signup from "./signup";
-import Resetpassword from "./reset-password";
-import UserAgreement from "./user-agreement";
-import Reset from "./reset-password/reset-password";
-import Home from "./home";
-import Dashboard from "./dashboard";
-import Confirmation from "./signup/authentication";
-import VerifyEmail from "./signup/verifyEmail";
+import React, { useEffect } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import Signin from './signin';
+import Signup from './signup';
+import Resetpassword from './reset-password';
+import UserAgreement from './user-agreement';
+import Reset from './reset-password/reset-password';
+import Home from './home';
+import Dashboard from './dashboard';
+import Confirmation from './signup/authentication';
+import VerifyEmail from './signup/verifyEmail';
 
 const authObj = {
   isAuthenticated:
-    JSON.parse(window.sessionStorage.getItem("isAuthenticated")) || false,
+    JSON.parse(window.sessionStorage.getItem('isAuthenticated')) || false,
   authenticate(isLoggedBool) {
-    window.sessionStorage.setItem("isAuthenticated", isLoggedBool);
-    let token = window.sessionStorage.getItem("token");
+    window.sessionStorage.setItem('isAuthenticated', isLoggedBool);
+    let token = window.sessionStorage.getItem('token');
     this.isAuthenticated =
-      token && JSON.parse(window.sessionStorage.getItem("isAuthenticated"));
+      token && JSON.parse(window.sessionStorage.getItem('isAuthenticated'));
   },
 };
 
@@ -36,14 +36,18 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 );
 
 export default function App() {
+  const [hold, setHold] = React.useState(0);
   useEffect(() => {
     axios
-      .get("https://hypaiqstyles.cyb.co.uk:8080/uiobjects/styles")
+      .get('https://hypaiqstyles.cyb.co.uk:8080/uiobjects/styles')
       .then((res) => {
-        console.log(res.data);
-        window.sessionStorage.setItem("styles", JSON.stringify(res.data));
+        window.sessionStorage.setItem('styles', JSON.stringify(res.data));
+        setHold(1);
       });
   }, []);
+
+  if (!hold) return <>Loading ... </>;
+
   return (
     <div>
       <Route
